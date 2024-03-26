de29d6ce72464d2017a64bd020ac6cb24c3dac98
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-26 14:39:19 +0100

    Sopshicated upgrade

--- bot.py
+++ bot.py
@@ -2,22 +2,56 @@ import requests
 import json
 import random
 import string
-from concurrent.futures import ThreadPoolExecutor
+import time
+from concurrent.futures import ThreadPoolExecutor 
+# Global variables for session persistence
+session_id = None
+
+def load_user_agents():
+    with open('data/userAgents.json', 'r') as f:
+        user_agents_data = json.load(f)
+        return user_agents_data['user_agents']
+
+user_agents = load_user_agents()
 
 def generate_random_string(length):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
+
 def send_post_request(url):
+    global session_id  # Access the global session ID
+
+    # Mimic legitimate user agents
+    headers = {
+        'User-Agent': random.choice(user_agents)
+    }
+
+    # Add session ID to headers if available
+    if session_id:
+        headers['Session-ID'] = session_id
+
     email = generate_random_string(8) + "@faucetearner.org"
     password = generate_random_string(10)
     payload = {'email': email, 'password': password}
-    print ("Sent: " + payload['email'])
+    print("Sent: " + payload['email'])
     try:
-        response = requests.post(url, json=payload)
-        print("Response:", response.json())
+        response = requests.post(url, json=payload, headers=headers)
+        # Check if session ID is present in response headers and update global variable
+        if 'Session-ID' in response.headers:
+            session_id = response.headers['Session-ID']
+        if response.status_code == 200:
+            print("Response:", response.json())
+        else:
+            print("Error: Status Code", response.status_code)
+            print("Response:", response.text)
     except Exception as e:
         print("Error occurred:", e)
     print("==============================================================================")
+    # Introduce random delay to simulate human behavior
+    delay = random.uniform(1, 10)
+    print("Waiting for "+delay"s")
+    time.sleep(delay)  # Random delay between 1 to 5 seconds
+
 
 if __name__ == "__main__":
     api_url = "https://faucetearner.org/api.php?act=login"
